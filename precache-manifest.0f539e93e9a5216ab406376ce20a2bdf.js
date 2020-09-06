ca48d7c19a4f276de60f88c69fd775e4e130681b
dujaejeong <gbtwld@gmail.com>
2020-09-07 03:07:04 +0900

    Updates

---
+++ precache-manifest.0f539e93e9a5216ab406376ce20a2bdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa17f6b2d2573b133ea7805a4ad187a7",
+    "url": "/dooravel_react/index.html"
+  },
+  {
+    "revision": "a873fa742a830be6bb18",
+    "url": "/dooravel_react/static/js/2.ecf6e0c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dooravel_react/static/js/2.ecf6e0c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a1038268e1ca23e9c07",
+    "url": "/dooravel_react/static/js/main.3b3b0ff2.chunk.js"
+  },
+  {
+    "revision": "d90f21ace1db0aed8044",
+    "url": "/dooravel_react/static/js/runtime-main.6eb3527e.js"
+  }
+]);
