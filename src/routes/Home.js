d4b9abaaa3140c0585309b3e1017ec5bf4bc2e8a
dujaejeong <gbtwld@gmail.com>
2020-09-07 04:31:14 +0900

    Corret deploy error

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,7 +8,7 @@ function Home() {
     <>
       <Header />
       <main>
-        <img src="/images/1.jpeg" />
+        <img src="../assets/images/1.jpeg" />
         <h3>무슨일이 일어난겨</h3>
       </main>
       <Footer />
